5b383e136f62535b02fff6780d868fff1f0abc24
Sven Balint <75448464+SBalint959@users.noreply.github.com>
2022-11-05 23:24:34 +0100

    sql Inj bez checkboxa

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,14 @@
+function funPretraga(){
+   const id = document.getElementById("pretraga").value;
+   console.log(id)
+   axios.post(window.location.protocol + "/sqlInjection/podaci", {id:id})
+   .then((result) => {
+   for (let i = 0; i < result.data.length; i++) {
+      document.getElementById("myData" + String(i+1)).innerHTML = JSON.stringify(result.data[i])
+   }
+   
+   })
+   .catch((err) => console.log(err))
 
+};
 
--- routes/sqlInjection.routes.js
+++ routes/sqlInjection.routes.js
@@ -1,28 +1,36 @@
 const express = require('express');
+const { restart } = require('nodemon');
 const router = express.Router();
 const db = require('../db');
 
 
 router.get('/', async function (req, res, next) {
     const sqlPodaciA = `SELECT * FROM GroupA`;
-    const sqlPodaciB = `SELECT * FROM GroupB`;
     try {
         const resultPodaciA = (await db.query(sqlPodaciA, [])).rows;
-        const rawPodaciA = JSON.stringify(resultPodaciA)
+        const rawPodaciA = JSON.stringify(resultPodaciA);
         const jsonPodaciA = JSON.parse(rawPodaciA);
-        const resultPodaciB = (await db.query(sqlPodaciB, [])).rows;
-        const rawPodaciB = JSON.stringify(resultPodaciB)
-        const jsonPodaciB = JSON.parse(rawPodaciB);
 
 
         res.render('sqlInjection', {
             title: 'SQL Injection',
             podaciA: jsonPodaciA,
-            podaciB: jsonPodaciB
         });
     } catch (err) {
         console.log(err);
     }
     
 });
+
+router.post('/podaci', async function (req, res, next) {
+    const {id} = req.body;
+    try {
+        const query = `SELECT username, email FROM Users WHERE userid = ${id}`;
+        const result = (await db.query(query, [])).rows;
+        res.status(200)
+        return res.json(result)
+    } catch(err) {
+        console.log(err)
+    }
+})
 module.exports = router;
