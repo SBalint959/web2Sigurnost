cae7bb5f32efd1b3e16ac465711d1e5399042d29
Sven Balint <75448464+SBalint959@users.noreply.github.com>
2022-11-06 14:01:07 +0100

    Sql injection done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,49 @@
+let protection = "OFF"
+
 function funPretraga(){
-   const id = document.getElementById("pretraga").value;
-   console.log(id)
-   axios.post(window.location.protocol + "/sqlInjection/podaci", {id:id})
-   .then((result) => {
-   for (let i = 0; i < result.data.length; i++) {
-      document.getElementById("myData" + String(i+1)).innerHTML = JSON.stringify(result.data[i])
+   for (let i = 0; i < 6; i++) {
+      document.getElementById("myData" + String(i+1)).innerHTML = ""
    }
-   
-   })
-   .catch((err) => console.log(err))
+   const id = document.getElementById("pretraga").value;
+   if (protection == "OFF"){
+      console.log(protection)
+      axios.post(window.location.protocol + "/sqlInjection/podaci", {id:id})
+      .then((result) => {
+      for (let i = 0; i < result.data.length; i++) {
+         document.getElementById("myData" + String(i+1)).innerHTML = JSON.stringify(result.data[i])
+      }
+      
+      })
+      .catch((err) => console.log(err))
+   }
+   else {
+
+      console.log(protection)
+      if(!isNaN(id)) {
+         axios.post(window.location.protocol + "/sqlInjection/podaci", {id:id})
+         .then((result) => {
+            for (let i = 0; i < result.data.length; i++) {
+               document.getElementById("myData" + String(i+1)).innerHTML = JSON.stringify(result.data[i])
+            }
+         })
+         .catch((err) => {
+            console.log(err)
+         })
+      } else {
+         document.getElementById("myData1").innerHTML = "WRONG INPUT!"
+      }
+   }
 
 };
+
+function turnItOn() {
+   if(document.getElementById("switch1").checked) {
+      document.getElementById("message").textContent = "Protected"
+      protection = "ON"
+   }
+   else {
+      document.getElementById("message").textContent = "Unprotected"
+      protection = "OFF"
+   }
+}
 
