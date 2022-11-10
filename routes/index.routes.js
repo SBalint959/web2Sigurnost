b4b76045c7405ed011460e94524627ebae49606a
Sven Balint <75448464+SBalint959@users.noreply.github.com>
2022-11-10 02:25:20 +0100

    baseUrl nije radio

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -13,7 +13,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: 'OiFYeQ5aWqu6JcCw8emrky0KUZiHe5umRFba4nueblszsSQqhqhue_5iL6Z8YWW_',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://web2lab2sigurnost.onrender.com',
   clientID: 'np27BTsQ543kAdKVJC88ZBy5m8lEMG1Q',
   issuerBaseURL: 'https://dev-xxdzkni1.us.auth0.com'
 };
